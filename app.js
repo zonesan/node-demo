445031838afefa69f371e9276d2c5932cd3c38cb
Zonesan <san@zoobook.org>
2018-08-07 16:02:49 +0800

    blue ocean

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var app = express();
 app.get('/', function (req, res) {
     console.log("hostname: ",process.env.HOSTNAME);
     
-    var resp = "Hello forest on a green background.\n";
+    var resp = "Hello ocean on a blue background.\n";
     resp += "hostname: " + process.env.HOSTNAME + ", version: v2\n";
 
     //res.send("hostname: "+ process.env.HOSTNAME);
